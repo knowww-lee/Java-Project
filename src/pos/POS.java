f883fe53741035865da6370fb0cb22c93b2b43f0
Noli Licudo <nolilicudo@gmail.com>
2024-07-03 00:44:15 +0800

    Initial commit. Login UI

---
+++ src/pos/POS.java
@@ -0,0 +1,13 @@
+
+package pos;
+import ui.LogIn;
+public class POS {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LogIn login = new LogIn();
+        login.setVisible(true);
+    }
+    
+}
